148c1d037540ec77c20d23045c88e71a241af1f2
RogerdeLamoBlasi <roger.blasi@estudiant.enti.cat>
2026-02-24 22:32:59 +0100

    Comentarios y creación inicial de el canvas

--- Assets/Scripts/Pelota.cs
+++ Assets/Scripts/Pelota.cs
@@ -8,42 +8,52 @@ public class Pelota : MonoBehaviour
 
     void Start()
     {
-        Spawn();
+        // Inicia la pelota en el centro
+        Spawn(); 
     }
     void Update()
     {
+        // Mueve la pelota cada frame según su velocidad
         transform.position += new Vector3(velocidadX, velocidadY, 0) * Time.deltaTime;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Rebote contra paredes verticales (izquierda/derecha)
         if (collision.CompareTag("Vertical"))
         {
             velocidadX = -velocidadX;
         }
 
+        // Rebote contra paredes Horizontales (arriba)
         if (collision.CompareTag("Horizontal"))
         {
             velocidadY = -velocidadY;
         }
 
+        // Rebote contra Pala o Bloque
         if (collision.CompareTag("Pala") || collision.CompareTag("Block"))
         {
+            //Calcula la dirección de rebote basada en la posición relativa entre la pelota y el objeto colisionado
             float diffX = transform.position.x - collision.transform.position.x;
             float diffY = transform.position.y - collision.transform.position.y;
             float mag = Mathf.Sqrt(diffX * diffX + diffY * diffY);
             diffX /= mag;
             diffY /= mag;
 
+            //Si la pelota está casi centrada respecto al objeto, rebota verticalmente.
             if (Mathf.Abs(diffX) < 0.3f)
             {
                 velocidadX = 0;
                 velocidadY = velocidad;
             }
+            //Si la pelota está más a los lados, rebota en esa dirección.
             else
             {
                 velocidadX = diffX * velocidad;
                 velocidadY = diffY * velocidad;
             }
+
+            // Si colisiona con un bloque, destruye el bloque
             if (collision.CompareTag("Block"))
             {
                 Destroy(collision.gameObject);
--- Assets/Scripts/controlPala.cs
+++ Assets/Scripts/controlPala.cs
@@ -5,10 +5,12 @@ public class controlPala : MonoBehaviour
     public float speed = 3f;
     void Update()
     {
+        // Obtiene la entrada horizontal (A/D o flechas izquierda/derecha)
         float move = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * move * speed * Time.deltaTime);
     }
 
+    // Evita que la pala atraviese las paredes verticales
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Vertical"))
